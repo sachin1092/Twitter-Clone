6e50a589d39f08d1cf2e30a8e62ad89a9172ae1a
sachin1092 <me@sachinshinde.com>
2014-12-24 23:19:05 +0530

    Refactoring and bootstraping

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -12,6 +12,10 @@ module SessionsHelper
 		@current_user ||= user_from_remember_token
 	end
 
+	def current_user?(user)
+		user == current_user
+	end
+	
 	def signed_in?
 		!current_user.nil?
 	end
@@ -26,9 +30,6 @@ module SessionsHelper
 		current_user = nil
 	end
 
-	def current_user?(user)
-		user == current_user
-	end
 
 	def deny_access
 		store_location
